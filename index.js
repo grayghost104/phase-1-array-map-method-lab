a16f9099146c110fe70ba7ae835bb04e457330e5
Gray Hanna <ghostblossom54288@gmail.com>
2024-06-04 16:18:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
+        'what does the this keyword mean?',
+     'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
@@ -9,8 +9,20 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+]
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () =>{
+  return tutorials.map((sentance) =>{
+    const word = sentance.split(" ")
+    const newOld = word.map((letter) => {
+      return letter[0].toUpperCase() + letter.slice(1);
+    })
+    return newOld.join(" ")
+  })
 }
+
+    
+       
+  
+
+
